e7e0cd6de7496cf619a108ca7d7ea258b56f2324
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2018-01-20 12:12:35 -0500

    adding google oauth and keys

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye : 'buddy'});
-});
+
+// google Client ID: 743189046153-sotf6ppbpok13saqco16ep8b9douncte.apps.googleusercontent.com
+// client secret: R_GLtTayv1ZZAKGOD0rjnsFx
+// initiates a new instance of GoogleStratey. Tells passport we want to authenticate with google.
+passport.deserializeUser(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
