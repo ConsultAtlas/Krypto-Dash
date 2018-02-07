0211e59a5a5b69a86fc49a5b879c3019416db3f1
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2018-02-06 20:23:23 -0500

    added toggle functionality to coin components on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,34 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Bitcoin from './coins/Bitcoin';
 import Ethereum from './coins/Ethereum';
 import Sia from './coins/Sia';
 
 
-const Dashboard = () => {
-    return(
-        <div>
-            <div className="divider"></div>
+class Dashboard extends Component {
+    
+    constructor () {
+        super()
+            this.state = {
+                bitcoinIsHidden: true,
+                ethereumIsHidden: true,
+                siaIsHidden: true
+            }
+        };
+        toggleBitcoinHidden () {
+            this.setState({
+                bitcoinIsHidden: !this.state.bitcoinIsHidden
+            });
+        };
+        toggleEthereumHidden () {
+            this.setState({
+                ethereumIsHidden: !this.state.ethereumIsHidden
+            });
+        };
+        toggleSiaHidden () {
+            this.setState({
+                siaIsHidden: !this.state.siaIsHidden
+            });
+        };
+    
+    render() {
+        return(
             <div>
-                <h3 className="center" style={{ marginTop: "50px", marginBottom: "100px"}}>Welcome Captain Picard!</h3>
-            </div>
-            <div className="center" style={{ marginTop: "50px", marginBottom: "50px"}}>
-                <div className="row center-align">
-                    <div className="col s4 center">
-                        <a className="waves-effect waves-light btn orange large">Bitcoin</a>
-                    </div>
-                    <div className="col s4 center">
-                        <a className="waves-effect waves-light btn orange large">Ethereum</a>
-                    </div>
-                    <div className="col s4 center">
-                        <a className="waves-effect waves-light btn orange large">Sia Coin</a>
+                <div className="divider"></div>
+                <div>
+                    <h3 className="center" style={{ marginTop: "50px", marginBottom: "75px"}}>Welcome Captain Picard!</h3>
+                </div>
+                <div className="center" style={{ marginTop: "50px", marginBottom: "75px"}}>
+                    <div className="row center-align">
+                        <div className="col s4 center">
+                            <a onClick={this.toggleBitcoinHidden.bind(this)} className="waves-effect waves-light btn orange large">Bitcoin</a>
+                        </div>
+                        <div className="col s4 center">
+                            <a onClick={this.toggleEthereumHidden.bind(this)} className="waves-effect waves-light btn orange large">Ethereum</a>
+                        </div>
+                        <div className="col s4 center">
+                            <a onClick={this.toggleSiaHidden.bind(this)} className="waves-effect waves-light btn orange large">Sia Coin</a>
+                        </div>
                     </div>
                 </div>
+                {!this.state.bitcoinIsHidden && <Bitcoin />}
+                {!this.state.ethereumIsHidden && <Ethereum />}
+                {!this.state.siaIsHidden && <Sia />}
             </div>
-            <Bitcoin />
-            <Ethereum />
-            <Sia />
-        </div>
-    );
+        );
+    }
 };
 
 export default Dashboard;
--- client/src/components/coins/Ethereum.js
+++ client/src/components/coins/Ethereum.js
@@ -7,18 +7,18 @@ class Ethereum extends Component {
     
         return (
            
-                <div class="col s12 m7">
-                    <div class="card horizontal">
-                    <div class="card-image">
+                <div className="col s12 m7">
+                    <div className="card horizontal">
+                    <div className="card-image">
                         <img src="https://s3.amazonaws.com/dcmedia3412/images/ethereum_150.png" />
                     </div>
-                    <div class="card-stacked">
-                        <div class="card-content">
-                        <h5 class="header">Ethereum (ETH)</h5>
+                    <div className="card-stacked">
+                        <div className="card-content">
+                        <h5 className="header">Ethereum (ETH)</h5>
                         <p style={{fontWeight: "600"}}>$0.021855 USD (17.66%)</p>
                         <p>0.00000288 BTC (9.68%)</p>
                         </div>
-                        <div class="card-action">
+                        <div className="card-action">
                         <a href="#">This is a link</a>
                         </div>
                     </div>
